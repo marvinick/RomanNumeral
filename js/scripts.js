cf352c3cff4c69c20e9a544d53849f62f420cd98
Marvin Nikijuluw & Micah Courey <student@epicodus.com>
2015-09-10 13:45:05 -0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+var romanNumeral = function(number) {
+  var roman = ["V"];
+
+  return 5;
+}
+
+// $(document).ready(function() {
+//   $("form#countupby").submit(function(event) {
+//     var count_by = parseInt($("input#number1").val());
+//     var count_to = parseInt($("input#number2").val());
+//     var result = countUpBy(count_by, count_to);
+//
+//     $(".number1").text(count_by);
+//     $(".number2").text(count_to);
+//     $(".result").text(result);
+//
+//     $("#result").show();
+//     event.preventDefault();
+//   });
+// });
---
+++ spec/specs.js
@@ -0,0 +1,10 @@
+describe('romanNumeral', function() {
+  it("checks if single digit is turned into roman", function() {
+    expect(countUpBy(5).to.eql('V');
+  })
+
+  // it("checks if count_by does not exceed equal to", function() {
+  // expect(countUpBy(10,104)).to.eql([10, 20, 30, 40, 50, 60, 70, 80, 90, 100]);
+  // })
+
+})
